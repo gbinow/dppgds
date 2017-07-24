1ead974ad2969647379dd3d82d5c77d9c6f91e72
Gustavo Binow <gbinow@gmail.com>
2017-07-24 19:32:00 -0300

    Incluir arquivos ajax

--- pgds/ajax/generate_hr_template.php
+++ pgds/ajax/generate_hr_template.php
@@ -4,8 +4,10 @@ require_once __DIR__ . '/base.php';
 
 $pdgsData = array_remove_empty($request);
 
+$jsonPath = '../output/'.$pdgsData['project'].'.json';
+file_put_contents($jsonPath , json_encode($pdgsData));
 
-file_put_contents(__DIR__. '/../output/pgds.json' , json_encode($pdgsData));
+shell_exec("/usr/bin/java -jar odtgen.jar $jsonPath");
 
 function array_remove_empty($haystack)
 {
